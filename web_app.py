2f4f212dce40e999e07230f418c2b65e31023f64
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-05-25 10:38:41 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -2,99 +2,188 @@
 # Setup and Design
 # ========================================
 import streamlit as st 
-import streamlit_theme as stt
-stt.set_theme({'primary': '#F63366'})
 import time
-from web_app_utils import *
-# ========================================
-# Stock Data Funktions
-# ========================================
+import pandas as pd
+import  pandas_datareader as pdr
+from pandas_datareader.yahoo.options import Options
+from yahoo_fin import stock_info as si
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import streamlit as st 
+from prophet import Prophet
+import prophet.plot as fplt
+from prophet.plot import plot_plotly, plot_components_plotly
+import datetime as dt
+from datetime import datetime
+import datetime
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from yahoo_fin.stock_info import get_quote_table
+from deep_translator import GoogleTranslator
+# ========================================    
+# Stock - Data
+# ========================================    
+class Stock:
+    def __init__(self, ticker, start=None, end=None):
 
+        self.ticker = ticker
+
+        try:
+            self._ticker = yf.Ticker(self.ticker)
+
+            if not (start or end):
+                self.df = self.df_ = self._ticker.history(period='max', auto_adjust=True)
+                
+            else:
+                self.df = self.df_ = self._ticker.history(start=start, end=end, auto_adjust=True)
+
+        except Exception as err:
+            print(err)
+# ========================================    
+# Know your Options 
+# ========================================    
+def create_spread_percent(df):
+    return (df.assign(spread_pct = lambda df: df.spread / df.ask))
+def filter_by_moneyness(df, pct_cutoff=0.2):
+    crit1 = (1-pct_cutoff)*df.strike < df.stockPrice
+    crit2 = df.stockPrice < (1+pct_cutoff)*df.strike
+    return (df.loc[crit1 & crit2].reset_index(drop=True))
+def call_options(sym):
+    stk = Options(sym)
+    dates = stk.expiry_dates
+    calls_merged = []
+    for d in dates:
+            calls_ = stk.get_call_data(expiry=d)
+            calls_filtered = calls_[['Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Last_Trade_Date', 'Root']].reset_index()
+            calls_filtered = calls_filtered [['Strike', 'Expiry', 'Type','Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Root']]
+            calls_filtered.columns =  [['Strike', 'Expiry', 'Type','Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Symbol']]     
+            calls_merged.append(calls_filtered)   
+    calls_df = pd.concat(calls_merged, axis=0) 
+    return calls_df
+def put_options(sym):
+    stk = Options(sym)
+    dates = stk.expiry_dates
+    puts_merged = []
+    for d in dates:
+            puts_ = stk.get_put_data(expiry=d)
+            puts_filtered = puts_[['Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Last_Trade_Date', 'Root']].reset_index()
+            puts_filtered = puts_filtered[['Strike', 'Expiry', 'Type','Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Root']]
+            puts_filtered.columns =  [['Strike', 'Expiry', 'Type','Last', 'Bid', 'Ask', 'Chg', 'Vol', 'Open_Int', 'IV', 'Underlying_Price', 'Symbol']]    
+            puts_merged.append(puts_filtered)
+    puts_df = pd.concat(puts_merged, axis=0) 
+    return puts_df
+def options_chain(symbol):
+    tk = yf.Ticker(symbol)
+    info = get_quote_table(symbol)
+    current_price = info["Quote Price"]
+    exps = tk.options
+    options = pd.DataFrame()
+    for e in exps:
+        opt = tk.option_chain(e)
+        opt = pd.DataFrame().append(opt.calls).append(opt.puts)
+        opt['expirationDate'] = e
+        options = options.append(opt, ignore_index=True)
+    options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days = 1)
+    options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
+    options['CALL'] = options['contractSymbol'].str[4:].apply(
+        lambda x: "C" in x)
+    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+    options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate the midpoint of the bid-ask
+    options['spread'] = (options['ask'] - options['bid'])
+    options = create_spread_percent(options)
+    options['stockPrice'] = current_price
+    options['intrinicValue'] = (options['strike'] - current_price)
+    options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate'])
+    options = filter_by_moneyness(options)
+    options_filtered  = options [['stockPrice','contractSymbol', 'CALL', 'expirationDate', 'dte' ,'strike', 'lastPrice',  'bid', 'ask', 'mark', 'spread', 'spread_pct', 'volume', 'openInterest', 'impliedVolatility', 'inTheMoney', 'intrinicValue']]
+    options_filtered.columns =  [[ 'Kurswert', 'OptionTicker', 'Call=1, Put=0', 'Einlösetermin', 'Ablaufdatum', 'Basispreis', 'Last', 'Bid', 'Ask', 'Mark', 'Spread', 'Spreadanteil', 'Volumen', 'Open Intrest', 'IV', 'im Geld', 'Substanzwert']]   
+    return options_filtered 
+def Options_Chain(ticker):
+    opt_chain = options_chain(ticker)
+    return opt_chain
+def Option(ticker):
+    _call = call_options(ticker)
+    _put = put_options(ticker)
+    _df =  pd.concat([_call, _put],  
+                  keys = ['Call', 'Put'], ignore_index = True)
+    return _df
+# ========================================     
+# Economic Data Funktions
+# ========================================   
+def get_sp500_data():
+    sp500 = Stock("^GSPC").df.Close
+    return sp500
+def get_vix_data():
+    vix = Stock("^VIX").df.Close
+    return vix
+def get_dax_data():
+    dax = Stock("^GDAXI").df.Close
+    return dax
+def get_mdax_data():
+    mdax = Stock("^MDAXI").df.Close
+    return mdax
+def get_sdax_data():
+    sdax = Stock("^SDAXI").df.Close
+    return sdax
 def read_dax_ticker():
     dax = pd.read_csv('index_stocks/DAX.csv', index_col='Index')
     return dax
-
 def read_sp500_ticker():
     sp500 = pd.read_csv('index_stocks/GSPC.csv', index_col='Index')
     return sp500
-
 def get_stock_data():
     stock = Stock(ticker_input)
     return stock
-
 def dax_stock_data():
     stock = Stock(DAX_ticker)
     return stock
-
 def snp_stock_data():
     stock = Stock(SNP_ticker)
     return stock
-
-def internet_stock_data():
-    stock = Stock(internet_ticker)
-    return stock
-
-def software_stock_data():
-    stock = Stock(software_ticker)
-    return stock
-    
 def get_quote_data():
     quote = pdr.get_quote_yahoo(ticker_input)
-    return quote 
-  
-def get_company_data():
-    company = Company(ticker_input)
-    return company
-
+    return quote  
 def get_option_data():
     options_df = Options_Chain(SNP_ticker)
     return options_df
-
-
 # ========================================
 # Prophet
 # ========================================
-
 def prophet_df(stk_price):
     df=stk_price.reset_index()
     df = df[["Date","Close"]] 
     df = df.rename(columns = {"Date":"ds","Close":"y"}) 
     return df
-
 def predict_with_prophet():
     stk = get_stock_data()
     stk_df = stk.df["2010":]
     df = prophet_df(stk_df)
     return df
-
 def predict_with_prophet_snp():
     stk = snp_stock_data()
     stk_df = stk.df["2010":]
     df = prophet_df(stk_df)
     return df
-
 def predict_with_prophet_dax():
     stk = dax_stock_data()
     stk_df = stk.df["2010":]
     df = prophet_df(stk_df)
     return df
-
 def predict_with_prophet_internet():
     stk = internet_stock_data()
     stk_df = stk.df["2010":]
     df = prophet_df(stk_df)
     return df
-
 def predict_with_prophet_software():
     stk = software_stock_data()
     stk_df = stk.df["2010":]
     df = prophet_df(stk_df)
     return df
-
 # ========================================
 # Launche App
 # ========================================
-
 ticker_radio = st.sidebar.radio('Kapitalmarktanalyse', ('Dashboard', 'Aktienanalyse'))
 
 if ticker_radio == 'Dashboard':

359d1c5993f1f97924275e2c19a982cbf6db9472
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-06-06 11:03:30 +0200

    Update mocca.py

--- mocca.py
+++ mocca.py
@@ -1,12 +1,21 @@
 # ========================================
-# Setup and Design
+# Setup 
 # ========================================
+# import modules for web app 
+import base64
+import os
+import json
+import pickle
+import uuid
+import re
+# 
 import streamlit as st 
 import time
 import pandas as pd
 import pandas_datareader as pdr
 from pandas_datareader.yahoo.options import Options
 from yahoo_fin import stock_info as si
+from yahoo_fin.stock_info import get_quote_table
 import yfinance as yf
 import pyfolio as pf
 import numpy as np
@@ -22,9 +31,6 @@ from datetime import datetime
 from datetime import datetime, timedelta 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from yahoo_fin.stock_info import get_quote_table
-import warnings
-import base64
 # ========================================     
 # Data Funktions
 # ========================================   
@@ -63,6 +69,93 @@ def predict_with_prophet():
     df = prophet_df(stk_df)
     return df
 # ========================================
+# Download Button
+# ========================================
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    """
+    Generates a link to download the given object_to_download.
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
+    pickle_it (bool): If True, pickle file.
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+    """
+    if pickle_it:
+        try:
+            object_to_download = pickle.dumps(object_to_download)
+        except pickle.PicklingError as e:
+            st.write(e)
+            return None
+
+    else:
+        if isinstance(object_to_download, bytes):
+            pass
+
+        elif isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+
+        # Try JSON encode for everything else
+        else:
+            object_to_download = json.dumps(object_to_download)
+
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+    dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+
+    return dl_link
+
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+
+
+# ========================================
 # Launche App
 # ========================================
 # Create an instance of the app 
@@ -91,18 +184,29 @@ def main():
     st.write(df_perf_stats)
     st.write(fig)
     
-    
-    def download_link(object_to_download, download_filename, download_link_text):
-        if isinstance(object_to_download,pd.DataFrame):
-            object_to_download = object_to_download.to_csv(index=False)
-            b64 = base64.b64encode(object_to_download.encode()).decode()
-            return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+    if st.checkbox('Select a file to download'):
+        st.write('~> Use if you want to test uploading / downloading a certain file.')
 
-    
-    if st.button('Download Dataframe as CSV'):
-        tmp_download_link = download_link(df_perf_stats, ticker_input+'_df.csv', 'Click here to download your data!')
-        st.markdown(tmp_download_link, unsafe_allow_html=True)
-        
+        # Upload file for testing
+        folder_path = st.text_input('Enter directory: deafult .', '.')
+        filename = file_selector(folder_path=folder_path)
+
+        # Load selected file
+        with open(filename, 'rb') as f:
+            s = f.read()
+
+        download_button_str = download_button(s, filename, f'Click here to download {filename}')
+        st.markdown(download_button_str, unsafe_allow_html=True)
+
+        if st.checkbox('Show code example'):
+            code_text = f"""
+                        with open('{filename}', 'rb') as f:
+                            s = f.read()
+                        download_button_str = download_button(s, '{filename}', 'Click here to download {filename}')
+                        st.markdown(download_button_str, unsafe_allow_html=True)"""
+
+            st.code(code_text, language='python')
+
     
 if __name__ == '__main__':
     main()

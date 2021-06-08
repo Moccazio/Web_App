baf07f698c4dd99f47d20ef7641563d2cd4471bb
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-06-08 14:25:37 +0200

    Update mocca.py

--- mocca.py
+++ mocca.py
@@ -5,6 +5,11 @@ import os
 import numpy as np
 from PIL import  Image
 import streamlit as st
+import streamlit as st
+from streamlit.hashing import _CodeHasher
+import time
+import sqlite3
+from sqlite3 import Error
 import streamlit_theme as stt
 stt.set_theme({'primary': '#E694FF'})    
 # ========================================   
@@ -15,15 +20,22 @@ from pages import ticker, snp
 # ========================================
 # Main App
 # ========================================
-app = MultiPage()
 
-display = Image.open('Logo.png')
-display = np.array(display)
-col1, col2 = st.beta_columns(2)
-col1.image(display, width = 400)
-col2.title("Mocca App")
+def main():
+    st.set_page_config(layout="wide")
+    app = MultiPage()
 
-app.add_page("S&P 500 Ticker", snp.app)
-app.add_page("Company Ticker", ticker.app)
+    display = Image.open('Logo.png')
+    display = np.array(display)
+    col1, col2 = st.beta_columns(2)
+    col1.image(display, width = 100)
+    col2.title("Mocca App")
+    
+    app.add_page("Company Ticker", ticker.app)
+    #app.add_page("S&P 500 Ticker", snp.app)
 
-app.run()
+    app.run()
+
+if __name__ == '__main__':
+    main()
+    

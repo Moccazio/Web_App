67c15e2b1539402d2601123c9af8c253809c57ab
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-06-19 21:05:18 +0200

    Update mocca.py

--- mocca.py
+++ mocca.py
@@ -163,11 +163,10 @@ def main():
     app.add_page("Dashboard", app_dashboard.app)
 
     #Removing and add pages 
-    pages = {"Login": page_login,}
 
     create_connection(DATABASE_FILE_LOCATION) 
 
-    st.sidebar.title(":floppy_disk: Dashboard")
+    st.sidebar.title(":floppy_disk:")
 
     app.run()
     
@@ -176,117 +175,3 @@ def main():
 
 if __name__ == '__main__':
     main()    
-    
-DATABASE_FILE_LOCATION = os.getcwd()+"\pythonsqlite.db" 
-
-TABLE_DIC = {'stocks':'stocks','stock_trans':'stock_transaction'} 
-
-
-
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-
-        if (conn.execute("SELECT name FROM sqlite_master").fetchall() not in TABLE_DIC.values()):
-            print("Creating "+ str(TABLE_DIC.values()) +" Table")
-
-        conn.execute("""CREATE TABLE IF NOT EXISTS """ + TABLE_DIC['stock_trans'] + """ (    
-                    Stock                       TEXT     PRIMARY KEY     NOT NULL,
-                    Bought_Price                REAL                     NOT NULL,
-                    Currency                    TEXT                     NOT NULL,
-                    Fees                        REAL                     NOT NULL,
-                    Quantity                    REAL                     NOT NULL);""")
-    
-        conn.execute("""CREATE TABLE IF NOT EXISTS """ + TABLE_DIC['stocks'] + """ (
-                    Stock                       TEXT     PRIMARY KEY     NOT NULL,
-                    Bought_Price_Avg            REAL                     NOT NULL,
-                    Currency                    TEXT                     NOT NULL,
-                    Fees                        REAL                     NOT NULL,
-                    Quantity                    REAL                     NOT NULL);""")
-
-        print("Successfully created "+ str(TABLE_DIC.values()) +" Table")
-
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-
-class _SessionState:
-
-    def __init__(self, session, hash_funcs):
-        """Initialize SessionState instance."""
-        self.__dict__["_state"] = {
-            "data": {},
-            "hash": None,
-            "hasher": _CodeHasher(hash_funcs),
-            "is_rerun": False,
-            "session": session,
-        }
-
-    def __call__(self, **kwargs):
-        """Initialize state data once."""
-        for item, value in kwargs.items():
-            if item not in self._state["data"]:
-                self._state["data"][item] = value
-
-    def __getitem__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-        
-    def __getattr__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-
-    def __setitem__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-
-    def __setattr__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-    
-    def clear(self):
-        """Clear session state and request a rerun."""
-        self._state["data"].clear()
-        self._state["session"].request_rerun()
-    
-    def sync(self):
-        """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
-
-        # Ensure to rerun only once to avoid infinite loops
-        # caused by a constantly changing state value at each run.
-        #
-        # Example: state.value += 1
-        if self._state["is_rerun"]:
-            self._state["is_rerun"] = False
-        
-        elif self._state["hash"] is not None:
-            if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
-                self._state["is_rerun"] = True
-                self._state["session"].request_rerun()
-
-        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
-
-
-def _get_session():
-    session_id = get_report_ctx().session_id
-    session_info = Server.get_current()._get_session_info(session_id)
-
-    if session_info is None:
-        raise RuntimeError("Couldn't get your Streamlit Session object.")
-    
-    return session_info.session
-
-
-def _get_state(hash_funcs=None):
-    session = _get_session()
-
-    if not hasattr(session, "_custom_session_state"):
-        session._custom_session_state = _SessionState(session, hash_funcs)
-
-    return session._custom_session_state    

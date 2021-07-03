3e625c26cc4656f492ed2211ed28b3db75523dc2
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-07-03 23:07:02 +0200

    Update mocca.py

--- mocca.py
+++ mocca.py
@@ -20,11 +20,11 @@ def main():
     display = Image.open('Logo.png')
     display = np.array(display)
     col1, col2 = st.beta_columns(2)
-    col1.image(display, width = 150)
+    col1.image(display, width = 250)
     col2.subheader("Mocca App")
     
-    app.add_page("Ticker", ticker.app)
     app.add_page("Probabilistic Programming", probability.app)
+    app.add_page("Ticker", ticker.app)
 
     app.run()
 

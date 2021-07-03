88112c47897ad15c743f464d5cf429d79b061130
Moccazio <71945683+Moccazio@users.noreply.github.com>
2021-07-03 20:09:58 +0200

    Update mocca.py

--- mocca.py
+++ mocca.py
@@ -20,7 +20,7 @@ def main():
     display = Image.open('Logo.png')
     display = np.array(display)
     col1, col2 = st.beta_columns(2)
-    col1.image(display, width = 190)
+    col1.image(display, width = 200)
     col2.subheader("Mocca Web App")
     
     app.add_page("Ticker", ticker.app)
